fb942baebaef73b18faa7ffbe3244ef6de593393
OhadBaehr <33723454+OhadBaehr@users.noreply.github.com>
2020-08-02 23:16:01 +0300

    Update useAbuse.js

--- useAbuse.js
+++ useAbuse.js
@@ -3,7 +3,4 @@ const abuseReducer = (state, action) => ({
     ...state,
     ...(typeof action === "function" ? action(state) : action)
 })
-//  const [state,setState]=useAbuse({loading:12, hasFinished:1, visible:2})
-//  setState({visible:8})
-//  setState(prev => ({loading: prev.loading +5}))
 export const useAbuse = (initialState) => useReducer(abuseReducer, initialState)
